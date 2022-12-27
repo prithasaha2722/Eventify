7f1d558bdfbf7051e9d8cc525c018be3a0695c79
Soham Chakraborty <schakraborty8455@gmail.com>
2022-12-27 17:51:48 +0530

    UPdated backend

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///eventlabs.db"
+db.init_app(app)
+
+class Organizer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    orgname = db.Column(db.String, unique=True, nullable=False)
+    orgweb = db.Column(db.String)
+    venue = db.Column(db.String, nullable=False)
+    startdate = db.Column(db.String, nullable=False)
+    enddate = db.Column(db.String, nullable=False)
+    starttime = db.Column(db.String, nullable=False)
+    endtime = db.Column(db.String, nullable=False)
+    numberofattendee = db.Column(db.Integer)
+    logo = db.Column(db.String)
+    signature = db.Column(db.String)
+
+class ParticipantsRegistration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String)
+    phone = db.Column(db.Integer, unique=True, nullable=False)
+    walletaddress = db.Column(db.String, unique=True, nullable=False)
+    address= db.Column(db.String, nullable=False)
+
+class ParticipantsCheckIn(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String)
+    phone = db.Column(db.Integer, unique=True, nullable=False)
+    walletaddress = db.Column(db.String, unique=True, nullable=False)
+    address= db.Column(db.String, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
