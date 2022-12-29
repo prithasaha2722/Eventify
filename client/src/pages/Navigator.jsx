7bf86b735bd142c76cca65d10db28080373d0472
[Kaustav Giri] <[kaustavgiri2017@gmail.com]>
2022-12-30 02:02:13 +0530

    Working on Registration Form

--- client/src/pages/Navigator.jsx
+++ client/src/pages/Navigator.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Bannerify from "../components/Bannerify/Bannerify";
 import Details from "../components/EventLabs/Details/Details";
 import EventLabs from "../components/EventLabs/EventLabs";
+import CertificateTemplate from "../components/Form/CertificateTemplate";
+import RegistrationFrom from "../components/Form/RegistrationFrom";
 import Home from "../components/Home/Home";
 import Error from "./Error";
 
@@ -14,6 +16,8 @@ const Navigator = () => {
         <Route path="/bannerify" element={<Bannerify/>}/>
         <Route path="/eventLabs" element={<EventLabs />} />
         <Route path="/eventLabs/details" element={<Details />} />
+        <Route path="/eventLabs/certificateTemplate" element={<CertificateTemplate />} />
+        <Route path="/eventLabs/registrationForm" element={<RegistrationFrom />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </main>
