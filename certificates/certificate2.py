b2ede0b60bd05a2718eb36191d8cf3fdf11bc4c9
Pritha Saha <prithasaha@Prithas-MacBook-Air.local>
2022-12-29 18:23:08 +0530

    Updated

--- certificates/certificate2.py
+++ certificates/certificate2.py
@@ -8,29 +8,29 @@ def make_certificates(name,event,date,venue,desig,design,n1,n2):
     image_source = Image.open('cert/cert2.png')
     draw = ImageDraw.Draw(image_source)
     name_width, name_height = draw.textsize(name)
-    draw.text((720,546), name, fill=FONT_COLOR)
+    draw.text((720,546), name, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",90))
     event_width, event_height = draw.textsize(event)
-    draw.text((406,870), event, fill=FONT_COLOR)
+    draw.text((406,870), event, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     date_width, date_height = draw.textsize(date)
-    draw.text((1212,869), date, fill=FONT_COLOR)
+    draw.text((1212,869), date, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     venue_width, venue_height = draw.textsize(venue)
-    draw.text((739,867), venue, fill=FONT_COLOR)
+    draw.text((739,867), venue, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
 
     desig_width, desig_height = draw.textsize(desig)
-    draw.text((520,1191), desig, fill=FONT_COLOR)
+    draw.text((520,1191), desig, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     design_width, design_height = draw.textsize(design)
-    draw.text((1322,1197), design, fill=FONT_COLOR)
+    draw.text((1322,1197), design, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     n1_width, n1_height = draw.textsize(n1)
-    draw.text((488,1079), n1, fill=FONT_COLOR)
+    draw.text((488,1079), n1, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     n2_width, n2_height = draw.textsize(n2)
-    draw.text((1301,1081), n2, fill=FONT_COLOR)
+    draw.text((1301,1081), n2, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     #image_source.save("./out/" + name +".png")
     #print('Saving Certificate of:', name)
     image_source.show()
 
 if __name__ == "__main__":
     names = ["Soham Chakraborty", "Kaustav Giri", "Pritha Saha","Ujjaini Ray"]
-    event = "Devfest Kolkata 2023"
+    event = "Devfest 2k23"
     date = "28.12.2022"
     venue = "Taal Kutir Convention"
 
--- certificates/certificate3.py
+++ certificates/certificate3.py
@@ -8,34 +8,34 @@ def make_certificates(name,event,date,org,desig,design,n1,n2):
     image_source = Image.open('cert/cert3.png')
     draw = ImageDraw.Draw(image_source)
     name_width, name_height = draw.textsize(name)
-    draw.text((813,720), name, fill=FONT_COLOR)
+    draw.text((813,720), name, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",90))
     event_width, event_height = draw.textsize(event)
-    draw.text((696,919), event, fill=FONT_COLOR)
+    draw.text((696,919), event, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
     date_width, date_height = draw.textsize(date)
-    draw.text((1560,912), date, fill=FONT_COLOR)
+    draw.text((1560,912), date, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
     org_width, org_height = draw.textsize(org)
-    draw.text((1153,916), org, fill=FONT_COLOR)
+    draw.text((1153,916), org, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
     name_width, name_height = draw.textsize(name)
-    draw.text((1132,871), name, fill=FONT_COLOR)
+    draw.text((1132,871), name, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
 
     desig_width, desig_height = draw.textsize(desig)
-    draw.text((432,1220), desig, fill=FONT_COLOR)
+    draw.text((432,1220), desig, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
     design_width, design_height = draw.textsize(design)
-    draw.text((1404,1204), design, fill=FONT_COLOR)
+    draw.text((1404,1204), design, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
     n1_width, n1_height = draw.textsize(n1)
-    draw.text((433,1133), n1, fill=FONT_COLOR)
+    draw.text((433,1133), n1, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     n2_width, n2_height = draw.textsize(n2)
-    draw.text((1391,1123), n2, fill=FONT_COLOR)
+    draw.text((1391,1123), n2, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
     #image_source.save("./out/" + name +".png")
     #print('Saving Certificate of:', name)
     image_source.show()
 
 if __name__ == "__main__":
     names = ["Soham Chakraborty", "Kaustav Giri", "Pritha Saha","Ujjaini Ray"]
-    event = "Devfest Kolkata 2023"
+    event = "Metathon"
     date = "28.12.2022"
     #venue = "Taal Kutir Convention"
-    org = "Google"
+    org = "Presidency"
 
     desig = "Principal"
     design = "Student Head"
