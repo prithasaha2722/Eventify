d90b2db24ae2c2330de8b7baa5f09b7da7184cc5
Pritha Saha <prithasaha@Prithas-MacBook-Air.local>
2022-12-30 01:53:16 +0530

    Updated

--- tickets/ticket1.py
+++ tickets/ticket1.py
@@ -6,11 +6,11 @@ def make_tickets1(name,event,date,org,venue,email,phone,time):
     image_source = Image.open('tckts/tic1.png')
     draw = ImageDraw.Draw(image_source)
     name_width, name_height = draw.textsize(name)
-    draw.text((73,317), name, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
+    draw.text((73,317), name, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",20))
     event_width, event_height = draw.textsize(event)
-    draw.text((1127,293), event, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",70))
+    draw.text((1028,223), event, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",110))
     date_width, date_height = draw.textsize(date)
-    draw.text((1165,485), date, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",30))
+    draw.text((1172,465), date, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",20))
     venue_width, venue_height = draw.textsize(venue)
     draw.text((1049,386), venue, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",40))
     org_width, org_height = draw.textsize(org)
@@ -20,7 +20,7 @@ def make_tickets1(name,event,date,org,venue,email,phone,time):
     phone_width, phone_height = draw.textsize(phone)
     draw.text((74,427),phone, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",20))
     time_width, time_height = draw.textsize(time)
-    draw.text((1176,528),time, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",20))
+    draw.text((1173,494),time, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",20))
     image_source.show()
 
 if __name__ == "__main__":
