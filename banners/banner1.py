929ea50b169147d8c2cd4398311973c8447b18c9
Pritha Saha <prithasaha@Prithas-MacBook-Air.local>
2022-12-31 12:53:39 +0530

    B

---
+++ banners/banner1.py
@@ -0,0 +1,31 @@
+from PIL import Image,ImageDraw,ImageFont
+import pandas as pd
+FONT_COLOR = "#000000"
+temp = Image.open('ban/ban1.png')
+WIDTH, HEIGHT = temp.size
+def make_banners1(org,event,venue,date,time):
+    image_source = Image.open('ban/ban1.png')
+    draw = ImageDraw.Draw(image_source)
+    org_width, org_height = draw.textsize(org)
+    draw.text((1809,845), org, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",200))
+    event_width, event_height = draw.textsize(event)
+    draw.text((2385,1373), event, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",400))
+    date_width, date_height = draw.textsize(date)
+    draw.text((2958,2454), date, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",80))
+    venue_width, venue_height = draw.textsize(venue)
+    draw.text((2608,2244),venue, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",115))
+    time_width, time_height = draw.textsize(time)
+    draw.text((2968,2574),time, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",80))
+    image_source.show()
+
+if __name__ == "__main__":
+    names = ["Soham Chakraborty", "Kaustav Giri", "Pritha Saha","Ujjaini Ray"]
+    event = "Metathon"
+    date = "28.12.2022"
+    venue = "Taal Kutir Convention"
+    org = "Student Developers Student Clubs"
+    phone = "9800910906"
+    email = "prithasaha19@gmail.com"
+    time = "09:30 a.m."
+    make_banners1(org,event,venue,date,time)
+    print("Banners done.")
---
+++ banners/banner3.py
@@ -0,0 +1,31 @@
+from PIL import Image,ImageDraw,ImageFont
+import pandas as pd
+FONT_COLOR = "#000000"
+temp = Image.open('ban/ban3.png')
+WIDTH, HEIGHT = temp.size
+def make_banners3(org,event,venue,date,time):
+    image_source = Image.open('ban/ban3.png')
+    draw = ImageDraw.Draw(image_source)
+    org_width, org_height = draw.textsize(org)
+    draw.text((3009,845), org, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",185))
+    event_width, event_height = draw.textsize(event)
+    draw.text((3385,1373), event, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",440))
+    date_width, date_height = draw.textsize(date)
+    draw.text((538,1688), date, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",80))
+    venue_width, venue_height = draw.textsize(venue)
+    draw.text((3638,2344),venue, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",135))
+    time_width, time_height = draw.textsize(time)
+    draw.text((539,2020),time, fill=FONT_COLOR,font=ImageFont.truetype("Arial.ttf",80))
+    image_source.show()
+
+if __name__ == "__main__":
+    names = ["Soham Chakraborty", "Kaustav Giri", "Pritha Saha","Ujjaini Ray"]
+    event = "Metathon"
+    date = "28.12.2022"
+    venue = "Taal Kutir Convention"
+    org = "Student Developers Student Clubs"
+    phone = "9800910906"
+    email = "prithasaha19@gmail.com"
+    time = "09:30 a.m."
+    make_banners3(org,event,venue,date,time)
+    print("Banners done.")
