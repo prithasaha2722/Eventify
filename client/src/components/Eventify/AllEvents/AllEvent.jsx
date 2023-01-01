67e28738e60e08794b98135dca182f52645b1c58
[Kaustav Giri] <[kaustavgiri2017@gmail.com]>
2023-01-01 21:29:34 +0530

    Working on single event design

--- client/src/components/Eventify/AllEvents/AllEvent.jsx
+++ client/src/components/Eventify/AllEvents/AllEvent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Bg from "./Bg";
+import EventArea from "./EventArea";
 
 const AllEvent = () => {
   return (
     <div className="w-screen">
       <Bg />
+      <EventArea />
     </div>
   );
 };
--- client/src/components/Eventify/AllEvents/EventArea.jsx
+++ client/src/components/Eventify/AllEvents/EventArea.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Event from '../../UI/Event'
 
 const EventArea = () => {
   return (
-    <div>EventArea</div>
+    <div className='w-screen h-auto p-10 flex flex-wrap justify-around items-center '>
+        <Event/>
+    </div>
   )
 }
 
