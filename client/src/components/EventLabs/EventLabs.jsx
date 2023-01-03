f2324fd77997641c778cae5022adb69baf5e3375
[Kaustav Giri] <[kaustavgiri2017@gmail.com]>
2023-01-03 23:53:23 +0530

    Working on smart contract client linking

--- client/src/components/EventLabs/EventLabs.jsx
+++ client/src/components/EventLabs/EventLabs.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Home from "./Home/Home";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import {useDispatch} from 'react-redux'
 import {authActions} from '../../Store/index'
 import Web3 from 'web3'
+import {TransactionContext} from '../../context/TransactionContext'
 
 const EventLabs = () => {
   const dispatch = useDispatch();
+  const { connectWallet, wallet } = useContext(TransactionContext)
   const { loginWithPopup } = useAuth0();
   useEffect(() => {
     loginWithPopup();
     connectWallet();
+    dispatch(authActions.setWalletAddress(wallet))
     
   }, []);
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -19,11 +22,11 @@ const EventLabs = () => {
     dispatch(authActions.login({name: user.name, email: user.email, authenticated: isAuthenticated}))
   }
 
-  const connectWallet = async() =>{
-    const web3 = new Web3(window.ethereum)
-    const account = await window.ethereum.request({method: "eth_requestAccounts"});
-    dispatch(authActions.setWalletAddress(account[0]))
-  }
+  // const connectWallet = async() =>{
+  //   const web3 = new Web3(window.ethereum)
+  //   const account = await window.ethereum.request({method: "eth_requestAccounts"});
+  //   dispatch(authActions.setWalletAddress(account[0]))
+  // }
   return (
     <div>
       <Home />
