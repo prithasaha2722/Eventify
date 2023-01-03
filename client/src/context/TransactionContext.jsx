229ad2902fad7e437b2d52ee1461d317abc78293
Kaustav Giri <kaustavgiri2017@gmail.com>
2023-01-03 23:51:44 +0530

    Working on Smart Contract

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { contractABI, contractAddress } from "../constant";
 import { createContext } from "react";
+import { useDispatch } from "react-redux";
+import {authActions} from '../Store/index'
 
 export const TransactionContext = createContext();
 
@@ -24,8 +26,24 @@ const EthereumContract = () => {
 };
 
 export const TransactionProvider = ({ children }) => {
+    const[wallet,setWallet] = useState('')
+
+    const checkWalletConnection = async() => {
+        if(!ethereum) return alert("Please install metamask")
+
+        const accounts = await ethereum.request({method: "eth_accounts"});
+    }
+    const connectWallet = async() => {
+        if(!ethereum) return alert("Please install metamask")
+
+        const accounts = await ethereum.request({method: "eth_requestAccounts"});
+
+        setWallet(accounts[0])
+    }
+
+
     return (
-        <TransactionContext.Provider value={{value: 'test'}}>
+        <TransactionContext.Provider value={{connectWallet, wallet}}>
             {children}
         </TransactionContext.Provider>
     );
