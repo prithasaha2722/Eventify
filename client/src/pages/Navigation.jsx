7458f415d1e61a866621277ab88457f439030e95
[Kaustav Giri] <[kaustavgiri2017@gmail.com]>
2023-01-06 15:52:45 +0530

    Intregation left

---
+++ client/src/pages/Navigation.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AllEvent from "../components/Eventify/AllEvents/AllEvent";
+import Details from "../components/EventLabs/Details/Details";
+import EventLabs from "../components/EventLabs/EventLabs";
+import CheckInForm from "../components/Form/CheckInForm";
+import EventForm from "../components/Form/EventForm";
+import ParticipantForm from "../components/Form/ParticipantForm";
+import RegistrationFrom from "../components/Form/RegistrationFrom";
+import Error from "./Error";
+
+const Navigation = () => {
+  return (
+    <main className="overflow-x-hidden bg-black">
+      <Routes>
+        <Route path="/" element={<AllEvent />} />
+        <Route path="/new-Event" element={<EventLabs />} />
+        <Route path="/new-Event/details" element={<Details />} />
+        <Route
+          path="/new-Event/:eventId/registrationForm"
+          element={<RegistrationFrom />}
+        />
+        <Route path="/new-Event/registerEvent" element={<EventForm />} />
+        <Route
+          path="/events/:eventId/participationForm"
+          element={<ParticipantForm />}
+        />
+        <Route path="participants/:eventId/checkin" element={<CheckInForm />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default Navigation;
