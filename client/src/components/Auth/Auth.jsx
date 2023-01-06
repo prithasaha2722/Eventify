e9bab7d772fa3d7016acf0bd948a1fee07d9adfc
[Kaustav Giri] <[kaustavgiri2017@gmail.com]>
2023-01-06 23:16:13 +0530

    Working on submit URL

---
+++ client/src/components/Auth/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+        <div>Email</div>
+        <input placeholder='Enter your Email' />
+    </div>
+  )
+}
+
+export default Auth
--- client/src/components/Eventify/AllEvents/EventArea.jsx
+++ client/src/components/Eventify/AllEvents/EventArea.jsx
@@ -4,7 +4,7 @@ import Event from "../../UI/Event";
 const EventArea = () => {
   return (
     <div className="flex flex-row flex-wrap items-center 2xl:items-start mt-3 bg-white">
-      <h2 className="text-3xl 2xl:text-7xl ml-7 font-bold">Featured Events</h2>
+      <h2 className="text-3xl mt-4 2xl:text-6xl ml-7 font-bold">Featured Events</h2>
       <div className="w-screen h-auto p-10 flex flex-wrap justify-around items-center ">
         <Event />
         <Event />
